a538e8716a5305efcc449537465d86e90932bc39
Ambroise Couissin <ambroise.couissin@gmail.com>
2017-10-22 14:13:01 +0800

    init

---
+++ Liaison.cs
@@ -0,0 +1,10 @@
+﻿namespace Desmos.Models
+{
+    public class Liaison
+    {
+        public string Id { get; set; }
+        public string PropertyId { get; set; }
+        public string FromId { get; set; }
+        public string ToId { get; set; }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Desmos.Hosts.ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
